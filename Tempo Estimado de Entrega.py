55cb208ff862dff92ba0ad6fd2ab7c96d0d5186c
mvvenancio <136772651+mvvenancio@users.noreply.github.com>
2023-09-29 22:27:04 -0300

    Tempo Estimado de Entrega
    Arquivo em Python Tempo Estimado de Entrega.

---
+++ Tempo Estimado de Entrega.py
@@ -0,0 +1,5 @@
+nomeRestaurante = input("Digite o nome do restaurante: ")
+tempoEstimadoEntrega = int(input("Digite o tempo estimado de entrega em minutos: "))
+
+mensagem = f'O restaurante {nomeRestaurante} entrega em {tempoEstimadoEntrega} minutos.'
+print(mensagem)
